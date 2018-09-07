ee171bdaeb34ee292d9a4f537385814f6b7b3e93
maxatroshenko <maxatrr@gmail.com>
2018-09-08 00:57:31 +0300

    Products and categories connection

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+	protected $fillable = [
+		'name',
+		'slug',
+		'price',
+		'description'
+	];
+
 	public function getRouteKeyName()
 	{
 		return 'slug';
--- tests/Unit/Models/Categories/CategoryTest.php
+++ tests/Unit/Models/Categories/CategoryTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models\Categories;
 
 use App\Models\Category;
+use App\Models\Product;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
@@ -37,5 +38,16 @@ class CategoryTest extends TestCase
 		]);
 
 		$this->assertEquals($anotherCategory->name, Category::ordered()->first()->name);
+	}
+
+	public function test_category_has_many_products()
+	{
+		$category = factory(Category::class)->create();
+
+		$category->products()->save(
+			factory(Product::class)->create()
+		);
+
+		$this->assertInstanceOf(Product::class, $category->products()->first());
 	}
 }
