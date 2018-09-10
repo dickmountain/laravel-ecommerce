495934bbf4ac03908eec72ff6f4ef829515f3738
maxatroshenko <maxatrr@gmail.com>
2018-09-11 00:13:47 +0300

    Product variations

--- tests/Unit/Products/ProductTest.php
+++ tests/Unit/Products/ProductTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Products;
 
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\ProductVariation;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
@@ -24,6 +25,17 @@ class ProductTest extends TestCase
 			factory(Category::class)->create()
 		);
 
-		$this->assertInstanceOf(Category::class, $product->categories()->first());
+		$this->assertInstanceOf(Category::class, $product->categories->first());
+	}
+
+	public function test_has_many_variations()
+	{
+		$product = factory(Product::class)->create();
+
+		$product->variations()->save(
+			factory(ProductVariation::class)->create()
+		);
+
+		$this->assertInstanceOf(ProductVariation::class, $product->variations->first());
 	}
 }
