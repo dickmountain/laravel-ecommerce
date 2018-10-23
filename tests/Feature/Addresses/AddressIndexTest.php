c97227d522f2ca393259880bbdfb9be86c2ac82e
maxatroshenko <maxatrr@gmail.com>
2018-10-23 18:07:36 +0300

    Listing addresses

---
+++ tests/Feature/Addresses/AddressIndexTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Addresses;
+
+use App\Models\Address;
+use App\Models\User;
+use Tests\TestCase;
+
+class AddressIndexTest extends TestCase
+{
+    public function test_fails_if_not_authenticated()
+    {
+        $this->json('GET', 'api/addresses')
+            ->assertStatus(401);
+    }
+
+	public function test_shows_addresses()
+	{
+		$user = factory(User::class)->create();
+
+		$address = factory(Address::class)->create([
+			'user_id' => $user->id
+		]);
+
+		$this->jsonAs($user, 'GET', 'api/addresses')
+			->assertJsonFragment([
+				'id' => $address->id
+			]);
+	}
+}
