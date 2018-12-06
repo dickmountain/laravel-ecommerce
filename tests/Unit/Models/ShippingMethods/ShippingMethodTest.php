9d53af8648b8d312cb63c30d06c6083eb0908bda
maxatroshenko <maxatrr@gmail.com>
2018-12-06 16:10:57 +0300

    Shipping methods and countries hooked up

--- tests/Unit/Models/ShippingMethods/ShippingMethodTest.php
+++ tests/Unit/Models/ShippingMethods/ShippingMethodTest.php
@@ -3,11 +3,23 @@
 namespace Tests\Unit\Models\ShippingMethods;
 
 use App\Ecommerce\Money;
+use App\Models\Country;
 use App\Models\ShippingMethod;
 use Tests\TestCase;
 
 class ShippingMethodTest extends TestCase
 {
+
+	public function test_belongs_to_many_countries()
+	{
+		$shipping = factory(ShippingMethod::class)->create();
+		$shipping->countries()->attach(
+			factory(Country::class)->create()
+		);
+
+		$this->assertInstanceOf(Country::class, $shipping->countries->first());
+	}
+
     public function test_returns_money_instance_for_price()
     {
     	$shipping = factory(ShippingMethod::class)->create();
