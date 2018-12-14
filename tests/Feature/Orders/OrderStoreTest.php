c8ba36b029e17d34c3a8865d5b11724eb2fdc4ae
maxatroshenko <maxatrr@gmail.com>
2018-12-14 15:01:32 +0300

    Controller: order validation

---
+++ tests/Feature/Orders/OrderStoreTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature\Orders;
+
+use App\Models\Address;
+use App\Models\User;
+use Tests\TestCase;
+
+class OrderStoreTest extends TestCase
+{
+    public function test_fails_if_not_authenticated()
+    {
+        $this->json('POST', 'api/orders')
+            ->assertStatus(401);
+    }
+
+	public function test_requires_address()
+	{
+		$user = factory(User::class)->create();
+
+		$this->jsonAs($user, 'POST', 'api/orders')
+			->assertJsonValidationErrors(['address_id']);
+	}
+
+	public function test_requires_address_that_exists()
+	{
+		$user = factory(User::class)->create();
+
+		$this->jsonAs($user, 'POST', 'api/orders', [
+			'address_id' => 1
+		])->assertJsonValidationErrors(['address_id']);
+	}
+
+	public function test_requires_address_that_belongs_to_authenticated_user()
+	{
+		$user = factory(User::class)->create();
+
+		$address = factory(Address::class)->create([
+			'user_id' => factory(User::class)->create()->id
+		]);
+
+		$this->jsonAs($user, 'POST', 'api/orders', [
+			'address_id' =>$address->id
+		])->assertJsonValidationErrors(['address_id']);
+	}
+
+	public function test_requires_shipping_method()
+	{
+		$user = factory(User::class)->create();
+
+		$this->jsonAs($user, 'POST', 'api/orders')
+			->assertJsonValidationErrors(['shipping_method_id']);
+	}
+
+	public function test_requires_shipping_method_that_exists()
+	{
+		$user = factory(User::class)->create();
+
+		$this->jsonAs($user, 'POST', 'api/orders', [
+			'shipping_method_id' => 1
+		])->assertJsonValidationErrors(['shipping_method_id']);
+	}
+}
