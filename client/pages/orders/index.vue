a71a7e2a66646842794ed6e6bbddadc12e16b9db
maxatroshenko <maxatrr@gmail.com>
2019-01-22 00:37:59 +0300

    Client: Orders

--- client/pages/orders/index.vue
+++ client/pages/orders/index.vue
@@ -1,20 +1,21 @@
 <template>
 	<div class="section">
+		{{ orders }}
 		<div class="container is-fluid">
 			<div class="columns">
 				<div class="column is-12">
 					<h1 class="title is-4">Your orders</h1>
 
-					<article class="message">
+					<article class="message" v-if="orders.length">
 						<div class="message-body">
 							<table class="table is-hoverable is-fullwidth">
 								<tbody>
-									<Order />
+									<Order v-for="order in orders" :key="order.id" :order="order"/>
 								</tbody>
 							</table>
 						</div>
 					</article>
-					<p>
+					<p v-else>
 						You have no orders
 					</p>
 				</div>
@@ -27,9 +28,21 @@
 	import Order from '@/components/orders/Order';
 
 	export default {
-		name: "index",
+		name: 'index',
+		data () {
+			return {
+				orders: []
+			};
+		},
 		components: {
 			Order
+		},
+		async asyncData ({ app }) {
+			let response = await app.$axios.$get('orders');
+
+			return {
+				orders: response.data
+			}
 		}
 	};
 </script>
