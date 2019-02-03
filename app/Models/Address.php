266a14f049ac6647dde3ba8870d52927f71a2469
maxatroshenko <maxatrr@gmail.com>
2019-02-03 11:58:36 +0300

    Models that can have default property refactored to a trait

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Traits\CanBeDefault;
 use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
+	use CanBeDefault;
+
 	protected $fillable = [
 		'name',
 		'address',
@@ -14,24 +17,6 @@ class Address extends Model
 		'country_id',
 		'default'
 	];
-
-	public static function boot()
-	{
-		parent::boot();
-
-		static::creating(function ($address) {
-			if ($address->default) {
-				$address->user->addresses()->update([
-					'default' => false
-				]);
-			}
-		});
-	}
-
-	public function setDefaultAttribute($value)
-	{
-		$this->attributes['default'] = (($value === 'true' || $value) ? true: false);
-	}
 	
 	public function user()
 	{
